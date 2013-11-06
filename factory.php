c352230593e8b61eebaf0456803ddeb891528993
Priscilla <prissipix@gmail.com>
2013-11-06 14:33:33 -0800

    addition of factory hw

---
+++ factory.php
@@ -0,0 +1,39 @@
+<?php
+
+class Car
+{
+    private $vehicle_make;
+    private $vehicle_model;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicle_make = $make;
+        $this->vehicle_model = $model;
+    }
+
+    public function get_make_and_model()
+    {
+        return $this->vehicle_make . ' ' . $this->vehicle_model;
+    }
+}
+
+class CarFactory
+{
+    public static function create($make, $model)
+    {
+        return new Car($make, $model);
+    }
+}
+
+// this will create car factory object
+$saturn = CarFactory::create('Saturn', 'Sky');
+
+print_r($saturn->get_make_and_model()); 
+
+
+
+
+
+
+
+?>
