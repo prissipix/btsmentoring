0312274797d3152fbb4be49d34cc28a085ff9ddf
Priscilla <prissipix@gmail.com>
2013-11-19 06:15:10 -0800

    chnges to deck and cardgame

--- EnglishDeck.php
+++ EnglishDeck.php
@@ -3,22 +3,10 @@ include_once "Deck.php";
 include_once "Card.php"; 
 
 class EnglishDeck extends Deck
-{
-	private $suits = array('Clubs', 'Diamonds', 'Hearts', 'Spades');
-	private $cards = array(
-		'Ace'=> 1, 
-		2 => 2, 
-		3 => 3, 
-		4 => 4, 
-		5 => 5, 
-		6 => 6, 
-		7 => 7, 
-		8 => 8, 
-		9 => 9, 
-		10 => 10, 
-		'Jack' => 10, 
-		'Queen'=>10, 
-		'King'=>10);
+{   
+	
+ private $suits = array('Clubs', 'Diamonds', 'Hearts', 'Spades');
+	private $cards = array('Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King');
 
 	public function dealCard() {
 		return array_pop($this->arrCards);
@@ -43,12 +31,9 @@ class EnglishDeck extends Deck
 
 }
 
-$oBaraja = new EnglishDeck();
-$oBaraja->shuffleCards();
+//$oBaraja = new EnglishDeck();
+//$oBaraja->shuffleCards();
 //$oBaraja->listCards();
 
 //$oCarta  = $oBaraja->dealCard();
 //echo("La carta: " . $oCarta);
-
-?>
-
--- cardgame.php
+++ cardgame.php
@@ -3,9 +3,32 @@
 include_once "Card.php";
 include_once "EnglishDeck.php";
 
+
+echo "Simple Blanckjack<br>";
  
 $oBaraja = new EnglishDeck(); 
 $oBaraja->shuffleCards();
+
+
+function countHand($oCard1, $oCard2) {
+	return cardValue($oCard1) + cardValue($oCard2);
+
+}
+
+function cardValue($oCard1) {
+	$count = 0;
+	if ($oCard1->getFigure() == 'Jack' || 
+		$oCard1->getFigure() == 'Queen' ||
+		$oCard1->getFigure() == 'King') {
+			$count += 10;
+	} elseif ($oCard1->getFigure() == 'Ace') {
+		$count += 11;
+	} else{
+		$count += $oCard1->getFigure();
+	}
+
+	return $count;
+}
 
 
 //PLAYER 1
@@ -14,8 +37,6 @@ echo("Player one has " . $oP1card1);
 
 $oP1card2  = $oBaraja->dealCard();
 echo(" and a " . $oP1card2 );
-
-$oPlayer1 = $oP1card1 + $oP1card2;
 
 echo "<br>";
 
@@ -26,9 +47,20 @@ echo("Player two has " . $oP2card1);
 $oP2card2  = $oBaraja->dealCard();
 echo(" and a " . $oP2card2);
 
+
+$oPlayer1 = countHand($oP1card1, $oP1card2);
+$oPlayer2 = countHand($oP2card1, $oP2card2);
+
+echo "<br> La cuenta de la mano del player1 es: " . $oPlayer1;
+echo "<br> La cuenta de la mano del player2 es: " . $oPlayer2;
+
 //Player Variables when cards are added together
-$oPlayer1 = (string)$oP1card1 + (string)$oP1card2;
-$oPlayer2 = (string)$oP2card1 + (string)$oP2card2;
+//$oPlayer1 = (string)$oP1card1 + (string)$oP1card2;
+//$oPlayer2 = (string)$oP2card1 + (string)$oP2card2;
+
+
+//echo "<br>Pris " . $oPlayer1;
+//echo "<br>Isma " . $oPlayer2;
 
 
 /*$oBaraja->compare($oPlayer1, $oPlayer2) {
@@ -41,16 +73,30 @@ $oPlayer2 = (string)$oP2card1 + (string)$oP2card2;
 	}
 }*/
 
+//validation 
+/*
+/if ($oPlayer1 == $array[$Ace] + $array[$Ace]) {
+	$oPlayer1 - 10;
+}*/
+
 echo "<br />";
+
+
 
 if($oPlayer1 > $oPlayer2){
     echo "Player 1 wins";
 } else if ($oPlayer1 < $oPlayer2) {
     echo "Player 2 wins";
-} else {
+} else if ($oPlayer1 >= 22) {
+	echo "Player 1 is over 21 and looses";
+}else if ($oPlayer2 >= 22) {
+	echo "Player 2 is over 21 and looses";
+}else{
     echo "it's a tie";
 }
 
+
+print_r($oPlayer1);
 
 //$oBaraja->foobar($oPlayer1, $oPlayer2);
 
